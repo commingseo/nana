eb609dafb066bf476cb906552d4b875d01db3237
commingseo <ouu123@naver.com>
2019-07-31 12:01:19 +0900

    last

--- halftone_project/settings.py
+++ halftone_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,3 +127,6 @@ STATIC_DIRS = [
     os.path.join(BASE_DIR, "post", "static"),
     os.path.join(BASE_DIR, "halftone_project", "static"),
 ]
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
